039eeb7dd70d25326562609ef3a79ea1e272bf01
Oguzhan Keskin <oguzhan.keskin@lcwaikiki.com>
2021-12-26 21:02:48 +0300

    BinaryGap solution added with 100% performance

---
+++ DeveloperTraining/Codility/Iterations/BinaryGap.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codility.Iterations
+{
+    public class BinaryGap
+    {
+        public static int Solution(int N)
+        {
+            var binaryGap = 0;
+            var binaryFormat = "";
+            var zeroCount = 0;
+
+            while (N > 0)
+            {
+                int remainder = N % 2;
+                binaryFormat = remainder.ToString() + binaryFormat;
+                N /= 2;
+            }
+
+            // or binaryFormat = Convert.ToString(N, 2);
+
+            var charArr = binaryFormat.ToCharArray();
+
+            for (int i = 0; i < charArr.Length; i++)
+            {
+                if (charArr[i] == '0')
+                    zeroCount++;
+                else
+                {
+                    if (zeroCount > binaryGap)
+                        binaryGap = zeroCount;
+
+                    zeroCount = 0;
+                }
+
+            }
+
+
+            return binaryGap;
+        }
+    }
+}
